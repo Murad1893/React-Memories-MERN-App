fe95a8a486b2474d11c423770f12c95ca35caa6d
Murad1893 <muradmansoor189@gmail.com>
2021-04-01 15:34:13 +0500

    Intialized project files

---
+++ server/index.js
@@ -0,0 +1,6 @@
+import express from "express"
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+// to enable this new IMPORT syntax we need to set type : module in package.json
