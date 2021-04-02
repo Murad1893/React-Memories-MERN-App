63d0869380fce4ad00e1e29c89ccf37ddea5d81f
Murad1893 <muradmansoor189@gmail.com>
2021-04-02 22:43:56 +0500

    Added UI for posts and individual posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,21 +2,26 @@ import React from 'react'
 import Post from './Post/Post'
 import useStyles from './styles'
 import { useSelector } from 'react-redux' // this will allow us to fetch data from the global redux store 
+import { Grid, CircularProgress } from '@material-ui/core'
 
 const Posts = () => {
 
   // this posts is the same name as in the combinedReducers
-  const posts = useSelector((state) => state.posts)
+  const posts = useSelector((state) => state.posts) // this will fectch all the posts from the store
   const classes = useStyles()
 
   console.log(posts)
 
   return (
-    <>
-      <h1> POSTS </h1>
-      <Post />
-      <Post />
-    </>
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   )
 }
 
